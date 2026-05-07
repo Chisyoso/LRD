c01a171b270f48eaedc2e3b6142ed528800d4e2d
CHM <132416621+Chisyoso@users.noreply.github.com>
2026-05-07 11:52:29 -0600

    Actualizar config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
   supabaseUrl: 'https://eithauomnpiwewekiepr.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpdGhhdW9tbnBpd2V3ZWtpZXByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNTY1NzMsImV4cCI6MjA5MzczMjU3M30.BGFRmoqE6fQgtqq3-my53klZr0-_WgzXl1tF5Eg6w3A',
-  siteName: 'Colegio Verde',
+  siteName: 'LRD',
   storageBuckets: {
     profiles: 'profile-images',
     posts: 'post-images',
